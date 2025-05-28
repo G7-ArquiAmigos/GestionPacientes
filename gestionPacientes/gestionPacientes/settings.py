846dcb04b8d581363b61b99e15698368f310966d
Angelokc01 <gamadavidjuanluis.ag@gmail.com>
2025-05-27 23:56:00 -0500

    ss

--- gestionPacientes/gestionPacientes/settings.py
+++ gestionPacientes/gestionPacientes/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sistemaHospitalario',
-    'paciente'
+    'paciente',
+    'gestionPacientes'
     ]
 
 MIDDLEWARE = [
