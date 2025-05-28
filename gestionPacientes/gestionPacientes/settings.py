9e5b9f1eb79e47e1709c3a634c3e2639d6fba490
Angel David Gonzales Moreno <88889693+Angelokc01@users.noreply.github.com>
2025-05-27 22:04:26 -0500

    Update settings.py
    Base de datos añadida de acuerdo a diagrama de despliegue

--- gestionPacientes/gestionPacientes/settings.py
+++ gestionPacientes/gestionPacientes/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'gestionPacientes.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'msd-diagnostico-paciente-db',
+         'USER': 'monitoring_user',
+         'PASSWORD': 'isis2503',
+         'HOST': '10.128.0.54',
+         'PORT': '5432',
+     }
+ }
 
 
 # Password validation
