8f78e56f853153df90f5686e62a72730a2fb3d2e
Angelokc01 <gamadavidjuanluis.ag@gmail.com>
2025-05-28 18:15:29 -0500

    sss

--- gestionPacientes/paciente/urls.py
+++ gestionPacientes/paciente/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pacientes_view, name='pacientes_view'),
+    path('pacientes/', views.pacientes_view, name='pacientes_view'),
     path('<int:pk>/', views.paciente_view, name='paciente_view'),
     path('analizar_eeg/<int:paciente_id>/', views.analizar_eeg_view)
     ]
