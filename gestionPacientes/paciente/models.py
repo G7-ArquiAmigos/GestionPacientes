56ecd546474f5bd718662602b26360c3bb92a424
Angelokc01 <gamadavidjuanluis.ag@gmail.com>
2025-05-27 23:05:43 -0500

    xd

--- gestionPacientes/paciente/models.py
+++ gestionPacientes/paciente/models.py
@@ -8,7 +8,6 @@ class Paciente(models.Model):
     genero = models.CharField(max_length=50)
     contactoEmergencia = models.CharField(max_length=50)
     estado = models.CharField(max_length=50)
-    sistemaHospitalario = models.ForeignKey(sistemaHospitalario, on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s' % (self.nombre)
